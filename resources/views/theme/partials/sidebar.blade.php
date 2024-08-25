ae6c3d77c32b44f776dbbb347e815680df1f8612
Anas Ashraf Fawzy Naguib <94796967+AnasAfawzy@users.noreply.github.com>
2024-08-25 23:22:43 +0300

    get recent blogs

--- resources/views/theme/partials/sidebar.blade.php
+++ resources/views/theme/partials/sidebar.blade.php
@@ -1,5 +1,6 @@
 @php
     $catgory = App\Models\Category::all();
+    $recentblogs = App\Models\Blog::latest()->take(3)->get();
 @endphp
 <div class="col-lg-4 sidebar-widgets">
     <div class="widget-wrap">
@@ -43,55 +44,27 @@
         </div>
 
         <div class="single-sidebar-widget popular-post-widget">
-            <h4 class="single-sidebar-widget__title">Recent Post</h4>
+            <h4 class="single-sidebar-widget__title">Recent Blogs</h4>
             <div class="popular-post-list">
-                <div class="single-post-list">
-                    <div class="thumb">
-                        <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb1.png"
-                            alt="">
-                        <ul class="thumb-info">
-                            <li><a href="#">Adam Colinge</a></li>
-                            <li><a href="#">Dec 15</a></li>
-                        </ul>
-                    </div>
-                    <div class="details mt-20">
-                        <a href="blog-single.html">
-                            <h6>Accused of assaulting flight attendant miktake alaways</h6>
-                        </a>
-                    </div>
-                </div>
-                <div class="single-post-list">
-                    <div class="thumb">
-                        <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb2.png"
-                            alt="">
-                        <ul class="thumb-info">
-                            <li><a href="#">Adam Colinge</a></li>
-                            <li><a href="#">Dec 15</a></li>
-                        </ul>
-                    </div>
-                    <div class="details mt-20">
-                        <a href="blog-single.html">
-                            <h6>Tennessee outback steakhouse the
-                                worker diagnosed</h6>
-                        </a>
-                    </div>
-                </div>
-                <div class="single-post-list">
-                    <div class="thumb">
-                        <img class="card-img rounded-0" src="{{ asset('assets') }}/img/blog/thumb/thumb3.png"
-                            alt="">
-                        <ul class="thumb-info">
-                            <li><a href="#">Adam Colinge</a></li>
-                            <li><a href="#">Dec 15</a></li>
-                        </ul>
-                    </div>
-                    <div class="details mt-20">
-                        <a href="blog-single.html">
-                            <h6>Tennessee outback steakhouse the
-                                worker diagnosed</h6>
-                        </a>
-                    </div>
-                </div>
+                @if (count($recentblogs) > 0)
+                    @foreach ($recentblogs as $blog)
+                        <div class="single-post-list">
+                            <div class="thumb">
+                                <img class="card-img rounded-0" src="{{ asset('storage') }}/blogs/{{ $blog->image }}"
+                                    alt="">
+                                <ul class="thumb-info">
+                                    <li><a href="#">{{ $blog->user->name }}</a></li>
+                                    <li><a href="#">{{ $blog->created_at->format('d m Y') }}</a></li>
+                                </ul>
+                            </div>
+                            <div class="details mt-20">
+                                <a href="{{ route('blogs.show', ['blog' => $blog]) }}">
+                                    <h6>{{ $blog->name }}</h6>
+                                </a>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
